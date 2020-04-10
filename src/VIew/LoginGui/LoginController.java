688270c16fcb099a0f8622e54a919d1631464607
bergmanmartin <martinbergman97@gmail.com>
2020-04-10 11:45:33 +0200

    Update LoginController.java

--- src/VIew/LoginGui/LoginController.java
+++ src/VIew/LoginGui/LoginController.java
@@ -3,11 +3,16 @@ package VIew.LoginGui;
 import Model.Users.User;
 
 import javax.swing.*;
+import java.io.File;
 import java.util.ArrayList;
 
 public class LoginController {
     private LoginFrame loginFrame;
     private User user;
+    private ArrayList<User> userlist = new ArrayList<User>();
+    private String file;
+    private String filePath;
+
 
     public LoginController (){
         loginFrame = new LoginFrame(this);
@@ -15,6 +20,7 @@ public class LoginController {
     }
 
 
+    public void emptyUsername() {
         if (loginFrame.getUsernname().equals("")) {
             JOptionPane.showMessageDialog(null, "Invalid username");
         }
@@ -45,4 +51,21 @@ public class LoginController {
         return user;
     }
 
+
+
+    public String selectedImage() {
+        JFileChooser imageChooser = new JFileChooser();
+        int chosenImage = imageChooser.showOpenDialog(null);
+        if (chosenImage == JFileChooser.APPROVE_OPTION) {
+            try {
+                filePath = imageChooser.getSelectedFile().getAbsolutePath();
+
+            } catch (Exception e) {}
+        }
+        return filePath;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
