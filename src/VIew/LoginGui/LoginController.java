2741c4eb7a3aa9c1febe3ee488abd757658e4495
bergmanmartin <martinbergman97@gmail.com>
2020-04-10 11:45:26 +0200

    added

--- src/VIew/LoginGui/LoginController.java
+++ src/VIew/LoginGui/LoginController.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 public class LoginController {
     private LoginFrame loginFrame;
     private User user;
-    private ArrayList<String> userlist = new ArrayList<String>();
 
     public LoginController (){
         loginFrame = new LoginFrame(this);
@@ -16,22 +15,34 @@ public class LoginController {
     }
 
 
-    public void invalidUsername() {
         if (loginFrame.getUsernname().equals("")) {
             JOptionPane.showMessageDialog(null, "Invalid username");
         }
     }
 
-    public void addUserToList() {
-        for (String user : userlist) {
-            userlist.add(loginFrame.getUsernname());
+    public void invalidCharacters() {
+        String username = loginFrame.getUsernname();
+
+        for (int i = 0; i < username.length(); i++) {
+
         }
     }
+
+    public void selectImageMessage() {
+       // if ()
+    }
+
+
 
     public void usernameAlreadyExists() {
 
     }
 
+    public User createUser() {
+        User user = new User(loginFrame.getUsernname(), loginFrame.getImage());
 
+        userlist.add(user);
+        return user;
+    }
 
 }
