30e66260c21f7539f052503b2386a83268ef88ff
Mewken <56892754+Mewken@users.noreply.github.com>
2020-04-15 15:07:00 +0200

    Update LoginFrame.java

--- src/VIew/LoginGui/LoginFrame.java
+++ src/VIew/LoginGui/LoginFrame.java
@@ -20,8 +20,9 @@ import java.util.HashMap;
 
 /**
  * Class of the Loginframe with gui and functionality.
- * @version 1.0
+ *
  * @author martinbergman.
+ * @version 1.0
  */
 
 
@@ -48,7 +49,6 @@ public class LoginFrame extends JPanel {
     private boolean correctName = false;
 
 
-
     private JFrame loginFrame;
 
 
@@ -70,10 +70,8 @@ public class LoginFrame extends JPanel {
         loginFrame.setResizable(false);
 
 
-
         buttonActions();
     }
-
 
 
     /**
@@ -81,105 +79,102 @@ public class LoginFrame extends JPanel {
      */
 
 
+    private void settingImagePanel() {
+        pnlImage.setPreferredSize(new Dimension(120, 120));
+        pnlImage.setMaximumSize(new Dimension(120, 120));
+        pnlImage.setBorder(new LineBorder(Color.BLACK, 1));
 
-        private void settingImagePanel() {
-            pnlImage.setPreferredSize(new Dimension(120, 120));
-            pnlImage.setMaximumSize(new Dimension(120, 120));
-            pnlImage.setBorder(new LineBorder(Color.BLACK, 1));
-
-            pnlImage.add(imageLbl);
-
-
-            pnlImageBtn.setLayout(new BoxLayout(pnlImageBtn, BoxLayout.X_AXIS));
-            pnlImageBtn.setPreferredSize(new Dimension(100, 40));
-            pnlImageBtn.add(imageBtn);
-
-            add(Box.createVerticalStrut(40));
-            add(pnlImage);
-            add(Box.createVerticalStrut(20));
-            add(pnlImageBtn);
-        }
-
-        /**
-         * Method for constructing the "bottom half" of the login frame.
-         */
-        private void settingSignInPanel() {
-            pnlLoginTf.setLayout(new BoxLayout(pnlLoginTf, BoxLayout.X_AXIS));
-            pnlLoginTf.setPreferredSize(new Dimension(300, 40));
-            pnlLoginTf.setMaximumSize(new Dimension(300, 40));
-
-            loginTf.setPreferredSize(new Dimension(190, 20));
-            loginTf.setDocument(new  JTextFieldLimit(10));
-            pnlLoginTf.add(loginTf);
-            add(Box.createVerticalStrut(20));
-            add(pnlLoginTf);
-        }
-
-        public void settingButtonPanel() {
-            pnlLoginBtn.setLayout(new BoxLayout(pnlLoginBtn, BoxLayout.X_AXIS));
-            pnlLoginBtn.setPreferredSize(new Dimension(80, 80));
-            loginBtn.setPreferredSize(new Dimension(80, 40));
-            pnlLoginBtn.add(loginBtn);
-            add(Box.createVerticalStrut(20));
-            add(pnlLoginBtn);
-        }
+        pnlImage.add(imageLbl);
 
 
-        public void buttonActions() {
+        pnlImageBtn.setLayout(new BoxLayout(pnlImageBtn, BoxLayout.X_AXIS));
+        pnlImageBtn.setPreferredSize(new Dimension(100, 40));
+        pnlImageBtn.add(imageBtn);
 
-                imageBtn.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        setImageLbl();
-                    }
-                });
-
-
-                loginBtn.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        setUserName();
-                        try {
-                            proceedLogin();
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-
-                });
-        }
-
-
-            public void setUserName() {
-                if (controller.checkUsername(loginTf.getText()) == true) {
-                    correctName = true;
-                } else {
-                    JOptionPane.showMessageDialog(null, "Only numbers and letters are allowed in username.");
-                }
-            }
-
-
-            public void setImageLbl() {
-
-                String filepath = controller.selectedImage();
-
-                if (filepath == null) {
-                    return;
-                } else if (filepath.endsWith(".png") || filepath.endsWith(".jpg")) {
-                    ImageIcon userPicture = new ImageIcon(filepath);
-                    Image image = userPicture.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
-                    userPicture = new ImageIcon(image);
-                    imageLbl.setIcon(userPicture);
-                    correctImage = true;
-                } else {
-                    JOptionPane.showMessageDialog(null, "Only jpg or png fileformat.");
-                }
-            }
-
-    public String getUsernname() {
-            return loginTf.getText();
+        add(Box.createVerticalStrut(40));
+        add(pnlImage);
+        add(Box.createVerticalStrut(20));
+        add(pnlImageBtn);
     }
 
+    /**
+     * Method for constructing the "bottom half" of the login frame.
+     */
+    private void settingSignInPanel() {
+        pnlLoginTf.setLayout(new BoxLayout(pnlLoginTf, BoxLayout.X_AXIS));
+        pnlLoginTf.setPreferredSize(new Dimension(300, 40));
+        pnlLoginTf.setMaximumSize(new Dimension(300, 40));
+
+        loginTf.setPreferredSize(new Dimension(190, 20));
+        loginTf.setDocument(new JTextFieldLimit(10));
+        pnlLoginTf.add(loginTf);
+        add(Box.createVerticalStrut(20));
+        add(pnlLoginTf);
+    }
+
+    public void settingButtonPanel() {
+        pnlLoginBtn.setLayout(new BoxLayout(pnlLoginBtn, BoxLayout.X_AXIS));
+        pnlLoginBtn.setPreferredSize(new Dimension(80, 80));
+        loginBtn.setPreferredSize(new Dimension(80, 40));
+        pnlLoginBtn.add(loginBtn);
+        add(Box.createVerticalStrut(20));
+        add(pnlLoginBtn);
+    }
+
+
+    public void buttonActions() {
+
+        imageBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setImageLbl();
+            }
+        });
+
+
+        loginBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setUserName();
+                try {
+                    proceedLogin();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+        });
+    }
+
+
+    public void setUserName() {
+        if (controller.checkUsername(loginTf.getText()) == true) {
+            correctName = true;
+        } else {
+            JOptionPane.showMessageDialog(null, "Only numbers and letters are allowed in username.");
+        }
+    }
+
+    public void setImageLbl() {
+
+        String filepath = controller.selectedImage();
+
+        if (filepath == null) {
+            return;
+        } else if (filepath.endsWith(".png") || filepath.endsWith(".jpg")) {
+            ImageIcon userPicture = new ImageIcon(filepath);
+            Image image = userPicture.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+            userPicture = new ImageIcon(image);
+            imageLbl.setIcon(userPicture);
+            correctImage = true;
+        } else {
+            JOptionPane.showMessageDialog(null, "Only jpg or png fileformat.");
+        }
+    }
+
+    public String getUsernname() {
+        return loginTf.getText();
+    }
 
     private class JTextFieldLimit extends PlainDocument {
         private int limit;
@@ -189,7 +184,7 @@ public class LoginFrame extends JPanel {
             this.limit = limit;
         }
 
-        public void insertString( int offset, String  str, AttributeSet attr ) throws BadLocationException {
+        public void insertString(int offset, String str, AttributeSet attr) throws BadLocationException {
             if (str == null) return;
 
             if ((getLength() + str.length()) <= limit) {
@@ -199,25 +194,23 @@ public class LoginFrame extends JPanel {
     }
 
     //TEST
-    
-            public void proceedLogin() throws IOException {
 
-                if (correctName && correctImage == true) {
-                    loginFrame.setVisible(false);
+    public void proceedLogin() throws IOException {
 
-                    User user = new User(loginTf.getText(), userPicture);
+        if (correctName && correctImage == true) {
+            loginFrame.setVisible(false);
 
-                    //MainFrame mainFrame = new MainFrame(user);
+            User user = new User(loginTf.getText(), userPicture);
 
-                    Server server = new Server(2343);
-                    Client Client = new Client("127.0.0.1", 2343);
-                    //MainFrame mf = new MainFrame(user);
-                }
-            }
+            //MainFrame mainFrame = new MainFrame(user);
 
-
-
+            Server server = new Server(2343);
+            Client Client = new Client("127.0.0.1", 2343);
+            //MainFrame mf = new MainFrame(user);
+        }
     }
+
+}
 
 
 
