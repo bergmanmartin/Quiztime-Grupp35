48358150f33c52890c5429a7ad98de8518b7882d
MarkusGerdtsson <62750134+MarkusGerdtsson@users.noreply.github.com>
2020-04-28 07:37:48 +0000

    k

--- src/VIew/LoginGui/LoginFrame.java
+++ src/VIew/LoginGui/LoginFrame.java
@@ -1,14 +1,10 @@
 package VIew.LoginGui;
 
-import Controller.Client;
-import Controller.Server;
 import Model.Users.User;
-import SecondFrame.MainSecondFrame;
-import StartPageGUI.MainFrame;
+import VIew.SecondFrame.MainSecondFrame;
 
 
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
@@ -16,8 +12,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 /**
  * Class of the Loginframe with gui and functionality.
