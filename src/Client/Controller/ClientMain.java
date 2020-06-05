1e5d9f826f8e29ed0ead69b3aedd637b2d4e416e
MarkusGerdtsson <62750134+MarkusGerdtsson@users.noreply.github.com>
2020-06-05 08:28:39 +0000

    ,

---
+++ src/Client/Controller/ClientMain.java
@@ -0,0 +1,19 @@
+package Client.Controller;
+
+import Client.View.LoginGui.LoginController;
+
+/**
+ * @Created 11/02/2020
+ * @project P1
+ * @Markus Gerdtsson
+ */
+public class ClientMain {
+
+    public static void main(String[] args) {
+        new LoginController();
+        new LoginController();
+        new LoginController();
+
+
+    }
+}
